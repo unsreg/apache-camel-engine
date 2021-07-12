0db7891c85906920793edfe986baf84a5a5bc020
unsreg <unsreg@gmail.com>
2021-07-13 01:40:39 +0300

    Added example & code refactoring

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose up --build -d
+docker-compose -p "stack_apache-camel-engine" up --build -d
---
+++ example/cron-ping-compress-send_ftp-send_minio/docker-compose.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -p "stack_apache-camel-ping" up --build -d
